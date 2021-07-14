30f6f2864cfaff8107a25ac8320c8cea5ffd4eab
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-07-13 23:02:01 -0300

    Utilizando Set numa classe

---
+++ SetNoModelo/Aluno.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetNoModelo
+{
+    class Aluno
+    {
+        private string nome;
+
+        private int numeroMatricula;
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+
+        public int NumeroMatricula
+        {
+            get { return numeroMatricula; }
+            set { numeroMatricula = value; }
+        }
+        public Aluno(string nome, int numeroMatricula)
+        {
+            this.nome = nome;
+            this.numeroMatricula = numeroMatricula;
+        }
+
+        public override string ToString()
+        {
+            return $"[Nome: {nome}, Matrícula: {numeroMatricula}]"; 
+        }
+
+    }
+}
