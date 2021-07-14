d6f5c3f46c534bf7fd6e0387bcc25dcbc111e607
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-07-13 22:47:54 -0300

    Entendendo funcionamento dos Sets

---
+++ OPoderDosSets/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPoderDosSets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conjuntos são mais rápidos para fazer buscas por elementos
+            //ISet e HashSet são classes genericas dependendo que se seja colocado um tipo
+            ISet<string> alunos = new HashSet<string>();
+            alunos.Add("Vanessa Tonini");
+            alunos.Add("Ana Losnak");
+            alunos.Add("Rafael Nercessian");
+
+            Console.WriteLine(alunos);
+            Console.WriteLine(string.Join(", ", alunos));
+
+            alunos.Add("Priscila Stuani");
+            alunos.Add("Rafael Rollo");
+            alunos.Add("Fabio Gushiken");
+
+            Console.WriteLine(string.Join(", ", alunos));
+
+            // ordem
+
+
+            //removendo ana, adicionando marcelo
+            alunos.Remove("Ana Losnak");
+            alunos.Add("Marcelo Oliveira");
+            Console.WriteLine(string.Join(", ", alunos));
+
+            //adicionando elemento repetido
+            alunos.Add("Fabio Gushiken");
+
+            //ordenando
+            //alunos.Sort();
+
+            List<string> alunosEmLista = new List<string>(alunos);
+            alunosEmLista.Sort();
+            Console.WriteLine(string.Join(", ", alunosEmLista));
+
+            Console.ReadLine();
+        }
+
+      
+    }
+}
